4b79431262702875d6d7effe614ef026a1f79159
David Polania <davidpolaniaac@users.noreply.github.com>
2016-05-02 21:14:34 -0500

    Commit Inicial

---
+++ Parqueadero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Parqueadero.Startup))]
+namespace Parqueadero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
